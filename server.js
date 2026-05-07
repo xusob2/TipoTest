1d4e6f7cfc1767dec8428cbb14ba89c85e53cdfa
xusob2 <jesus_9291@hotmail.com>
2026-05-07 23:45:58 +0200

    scoreP

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path'); 
-const { Question, Score, Report } = require('./db');
+const { Question, Score, Report,PerfectRun } = require('./db');
 const app = express();
 const PORT = process.env.PORT || 3000; 
 
